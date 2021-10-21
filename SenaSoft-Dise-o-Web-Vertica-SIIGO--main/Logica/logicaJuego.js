db926184834e218ce1f7e2a029b62cf6192ebf53
unknown <1@IBADFPCCS10F055.FormacionSena.RED>
2021-10-21 08:56:06 -0500

    Commit Esconder

--- SenaSoft-Dise-o-Web-Vertica-SIIGO--main/Logica/logicaJuego.js
+++ SenaSoft-Dise-o-Web-Vertica-SIIGO--main/Logica/logicaJuego.js
@@ -1,6 +1,14 @@
 function iniciarJuego () {
-    var dato = document.getElementById("tabla1"+
-,"tabla2");
-    dato.style.visibility = "hidden";}
+    var dato = document.getElementById("tabla1");
+    var dato1 = document.getElementById("tabla2");
+    var dato2 = document.getElementById("tabla3");
+    var dato3 = document.getElementById("tabla4");
+    var dato4 = document.getElementById("tabla5");
+    dato.style.visibility = "hidden";
+    dato1.style.visibility = "hidden"
+    dato2.style.visibility = "hidden"
+    dato3.style.visibility = "hidden"
+    dato4.style.visibility = "hidden";}
+
 
 
